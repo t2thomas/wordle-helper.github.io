c332bfa9e1165eb6201fc5693de744e51902de87
t2thomas <105951331+t2thomas@users.noreply.github.com>
2022-05-29 17:41:27 -0700

    Script to reload cache

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,23 @@ let answerFileLocation = "../assets/wordle_answer_list_sorted.txt";
 let restingInstruction = "Complete atleast one word to be able to get your options. Tap on letters to change colors. "
 let awaitingConfirmationInstruction = "Tap on letters to change colors. Click on 'Next word' or hit the return key to continue entering letters. Click on 'Get Options' to get available word options."
 
+// Check if a new cache is available on page load.
+window.addEventListener('load', function(e) {
+
+  window.applicationCache.addEventListener('updateready', function(e) {
+    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+      // Browser downloaded a new app cache.
+      // Swap it in and reload the page to get the new hotness.
+      window.applicationCache.swapCache();
+      if (confirm('A new version of this site is available. Load it?')) {
+        window.location.reload();
+      }
+    } else {
+      // Manifest didn't changed. Nothing new to server.
+    }
+  }, false);
+
+}, false);
 
 window.addEventListener('load', function(event) {
   document.getElementById('next-word').disabled = true;
